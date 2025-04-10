ab3126a0a1032d68d2035eb7ee49c048cd5b5279
Ryan Stutz <ryan.stutz06@gmail.com>
2025-04-10 14:43:10 -0400

    Created node class; implemented populateGraph function which both creates the graph vertices and populates the edges between each vertices;

---
+++ kt.js
@@ -0,0 +1,70 @@
+class Node {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.edges = [];
+    }
+
+    // toString() {
+    //     console.log(`Node(${this.x}, ${this.y}) - Edges ${this.edges}`);
+    // }
+}
+
+function populateGraph() {
+
+
+    let nodeArray = [];
+
+    for(let i = 0; i < 8; i++) {
+        nodeArray.push([]);
+        for(let j = 0; j < 8; j++) {
+            nodeArray[i].push(new Node(i, j));
+        }
+    }
+
+    let currNode;
+    
+    for(let i = 0; i < nodeArray.length; i++) {
+        for(let j = 0; j < nodeArray[i].length; j++) {
+
+            currNode = nodeArray[i][j];
+
+            if((i + 1 < 8) && (j + 2 < 8)) {
+                currNode.edges.push(nodeArray[i+1][j+2]);
+            }
+
+            if((i + 1 < 8) && (j - 2 >= 0)) {
+                currNode.edges.push(nodeArray[i+1][j-2]);
+            }
+
+            if((i + 2 < 8) && (j + 1 < 8)) {
+                currNode.edges.push(nodeArray[i+2][j+1]);
+            }
+
+            if((i + 2 < 8) && (j - 1 >= 0)) {
+                currNode.edges.push(nodeArray[i+2][j-1]);
+            }
+
+            if((i - 1 >= 0) && (j + 2 < 8)) {
+                currNode.edges.push(nodeArray[i-1][j+2]);
+            }
+
+            if((i - 1 >= 0) && (j - 2 >= 0)) {
+                currNode.edges.push(nodeArray[i-1][j-2]);
+            }
+
+            if((i - 2 >= 0) && (j + 1 < 8)) {
+                currNode.edges.push(nodeArray[i-2][j+1]);
+            }
+
+            if((i - 2 >= 0) && (j - 1 >= 0)) {
+                currNode.edges.push(nodeArray[i-2][j-1]);
+            }
+        }
+    }
+    // console.log(nodeArray[1][2].edges)
+    return nodeArray;
+}
+
+let arr = populateGraph();
+console.log(arr);

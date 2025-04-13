6ca4e9736f6d7c994966dab9f46069beee68c101
Ryan Stutz <ryan.stutz06@gmail.com>
2025-04-13 14:10:02 -0400

    implemented knightMoves function. Function checks arguments and throws error if out of bounds. Otherwise, finds the shortest path from startObj to destObj and prints out the results

--- kt.js
+++ kt.js
@@ -3,13 +3,11 @@ class Node {
         this.x = x;
         this.y = y;
         this.edges = [];
+        this.predecessor = null;
     }
-
-    // toString() {
-    //     console.log(`Node(${this.x}, ${this.y}) - Edges ${this.edges}`);
-    // }
 }
 
+// Sets up the "graph" by creating nodes and populating their edge array according to the chess knight pieces possible movement
 function populateGraph() {
 
 
@@ -31,28 +29,16 @@ function populateGraph() {
 
             currNode = nodeArray[i][j];
 
-            if((i + 1 < 8) && (j + 2 < 8)) {
-                currNode.edges.push(nodeArray[i+1][j+2]);
-            }
-
-            if((i + 1 < 8) && (j - 2 >= 0)) {
-                currNode.edges.push(nodeArray[i+1][j-2]);
-            }
-
             if((i + 2 < 8) && (j + 1 < 8)) {
                 currNode.edges.push(nodeArray[i+2][j+1]);
             }
 
-            if((i + 2 < 8) && (j - 1 >= 0)) {
-                currNode.edges.push(nodeArray[i+2][j-1]);
+            if((i + 1 < 8) && (j + 2 < 8)) {
+                currNode.edges.push(nodeArray[i+1][j+2]);
             }
 
             if((i - 1 >= 0) && (j + 2 < 8)) {
                 currNode.edges.push(nodeArray[i-1][j+2]);
-            }
-
-            if((i - 1 >= 0) && (j - 2 >= 0)) {
-                currNode.edges.push(nodeArray[i-1][j-2]);
             }
 
             if((i - 2 >= 0) && (j + 1 < 8)) {
@@ -62,10 +48,71 @@ function populateGraph() {
             if((i - 2 >= 0) && (j - 1 >= 0)) {
                 currNode.edges.push(nodeArray[i-2][j-1]);
             }
+
+            if((i - 1 >= 0) && (j - 2 >= 0)) {
+                currNode.edges.push(nodeArray[i-1][j-2]);
+            }
+
+            if((i + 1 < 8) && (j - 2 >= 0)) {
+                currNode.edges.push(nodeArray[i+1][j-2]);
+            }
+
+            if((i + 2 < 8) && (j - 1 >= 0)) {
+                currNode.edges.push(nodeArray[i+2][j-1]);
+            }
         }
     }
     return nodeArray;
 }
 
+function knightMoves(startObj, destObj) {
+    if((startObj[0] > 7 || startObj[0] < 0 || startObj[1] > 7 || startObj[1] < 0) || (destObj[0] > 7 || destObj[0] < 0 || destObj[1] > 7 || destObj[1] < 0)) {
+        console.error("Out of bounds. Numbers must be between 0 and 7.");
+        return;
+    }
+
+
+    let newSet = new Set(); // To check if the node has been visited already
+    let queue = []; // To keep track of which node to visit next. Search algorithm uses BFS method
+    queue.push(arr[startObj[0]][startObj[1]]);
+    newSet.add(arr[startObj[0]][startObj[1]]);
+
+    let currNode;
+    let found = false;
+
+    /* currNode becomes the first element on the queue which is removed. currNodes edges are iterated over. If the edge node in question is not part of the set, it is added to the queue and set
+    and its predecessor becomes "currNode". We then check to see if the edge node is the one we are looking for. If it is, set found to true and exit the for loop. While loop ends afterwards.
+    */
+    while(!found) {
+        currNode = queue.shift();
+        for(let i = 0; i < currNode.edges.length; i++) {
+            if(!newSet.has(currNode.edges[i])) {
+                queue.push(currNode.edges[i]);
+                newSet.add(currNode.edges[i]);
+                currNode.edges[i].predecessor = currNode;
+            }
+            if(currNode.edges[i].x == destObj[0] && currNode.edges[i].y == destObj[1]) {
+                found = true;
+                break;
+            }
+        }
+    }
+
+    // Set currNode to target node from array and backtrack through predecessors, adding each to the retVal array. Finally, print out results of retVal.
+    let retVal = [];
+    currNode = arr[destObj[0]][destObj[1]];
+
+    while(currNode != null) {
+        retVal.unshift(currNode);
+        currNode = currNode.predecessor;
+    }
+
+    console.log(`You made it in ${retVal.length-1} moves! Heres your path:`)
+    for(let i = 0; i < retVal.length; i ++) {
+        console.log(`[${retVal[i].x}, ${retVal[i].y}]`);
+    }
+}
+
 let arr = populateGraph();
-console.log(arr);
+
+knightMoves([0,0], [7,7]);
